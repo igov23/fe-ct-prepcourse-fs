5752f04d7b0e32f085dfa77a220b2f2c50f3f27b
ifed <antwars@gmail.com>
2023-11-11 13:50:17 -0600

    M03 Completado

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,60 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   return x == y;
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   return str1.length === str2.length;
 }
-
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+
+   return num < 90;
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+
+   return num > 50;
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
-}
+   if(num == 0 || num == 2 || num == 4 || num == 6 || num ==8)
+   {
+       return true;
+     
+   }
+       else {
+        return false;
+      }
+   }
+
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+
+   if(num == 1 || num == 3 || num == 5 || num == 7 || num ==9)
+   {
+       return true;
+     
+   }
+       else {
+        return false;
+      
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
